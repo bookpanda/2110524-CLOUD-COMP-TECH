a0f6445ec6ddd08ad911a81304486a4089334702
Idhibhat Pankam <marginpankam@gmail.com>
2025-01-24 23:36:58 +0700

    w

--- activity/week3/siege_runner.sh
+++ activity/week3/siege_runner.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 concurrency=10  # Starting value for concurrency (-c)
-delay=1         # Delay between requests (-d)
-repeats=5       # Number of repetitions (-r)
+delay=0.01         # Delay between requests (-d)
+repeats=100       # Number of repetitions (-r)
 url="http://act3-paas-autoscale-5-env.eba-4ifhnppy.ap-southeast-1.elasticbeanstalk.com"
 log_file="siege.log"
 
-for i in {1..20}; do
+for i in {1..24}; do
     echo "Running siege with concurrency=$concurrency, delay=$delay, repeats=$repeats..."
     
     siege -c$concurrency -d$delay -r$repeats "$url" -l \
