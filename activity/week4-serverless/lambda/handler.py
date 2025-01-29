b00eba4dfccbd3cb415fce6ae312140b1de198dc
Idhibhat Pankam <marginpankam@gmail.com>
2025-01-29 16:47:13 +0700

    s

---
+++ activity/week4-serverless/lambda/handler.py
@@ -0,0 +1,31 @@
+import json
+
+
+def lambda_handler(event, context):
+    # TODO implement
+    try:
+        body = json.loads(event["body"])
+        a = body.get("a", "9")
+        b = body.get("b", "6")
+        op = body.get("op", "+")
+
+        result = a
+        match op:
+            case "+":
+                result += b
+            case "-":
+                result -= b
+            case "*":
+                result *= b
+            case "/":
+                result /= b
+
+        return {"statusCode": 200, "body": json.dumps({"result": result})}
+
+    except json.JSONDecodeError:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": "Invalid JSON in body"}),
+        }
+    except Exception as e:
+        return {"statusCode": 500, "body": json.dumps({"error": str(e)})}
